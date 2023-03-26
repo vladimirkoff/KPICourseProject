f194075b85a1778b4fd29a4062220ec3ad5c6218
DanyloM73 <danyamalanin7@gmail.com>
2023-03-26 13:25:09 +0300

    Changes: - add layout for `main` and `slot` page - update `main.js` and `slots.js`

--- main.js
+++
@@ -1,9 +0,0 @@
-let money = localStorage.getItem('money');
-if (!money || money == NaN) {
-  money = 0;
-}
-
-localStorage.setItem('money', money);
-document.querySelector('#money > p').innerHTML = money;
-
-
