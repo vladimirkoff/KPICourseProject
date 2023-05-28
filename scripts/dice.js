1995e4ca6ff882a45f7dae3dedd4bd845b355fb9
Vladimir <vovakovalev2016@icloud.com>
2023-05-28 14:32:34 +0300

    Add new test for dice implementation

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,3 +1,27 @@
+'use strict'
+
+const score0El = document.getElementById("score_0");
+const score1El = document.getElementById("score_1");
+const diceEl = document.querySelector(".dice");
+
+const btnRoll = document.querySelector(".btn_roll")
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+btnRoll.addEventListener("click", function() {
+    diceEl.classList.remove("hidden");
+    const dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+});
+
+
+
+
+
+
+
 function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
