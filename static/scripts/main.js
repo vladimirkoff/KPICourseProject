b4962528e5c71a605ce3b3ae7712286b5dd1f7c7
ipassternak <retriumpf@gmail.com>
2023-06-02 19:51:26 +0300

    Hot-fix

--- static/scripts/main.js
+++
@@ -1,7 +0,0 @@
-let money = localStorage.getItem('money');
-if (!money || isNaN(money)) {
-  money = 0;
-}
-
-localStorage.setItem('money', money);
-document.querySelector('#money > p').innerHTML = money;
